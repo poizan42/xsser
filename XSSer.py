776124380d4883c8c8ca7d93c523a97e853a9156
lordepsylon <lordepsylon@ff403fe6-d2d6-42a6-870c-9d2f445c561c>
2010-04-19 00:37:56 +0000

    xsser-0.5a released
    git-svn-id: svn://svn.code.sf.net/p/xsser/code@6 ff403fe6-d2d6-42a6-870c-9d2f445c561c

--- XSSer.py
+++ XSSer.py
@@ -1,25 +1,6 @@
-#!/usr/bin/python -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-"
 # vim: set expandtab tabstop=4 shiftwidth=4:
-"""
-$Id$
-
-This file is part of the xsser project, http://xsser.sourceforge.net.
-
-Copyright (c) 2010 psy <root@lordepsylon.net>
-
-xsser is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free
-Software Foundation version 3 of the License.
-
-xsser is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-details.
-
-You should have received a copy of the GNU General Public License along
-with xsser; if not, write to the Free Software Foundation, Inc., 51
-Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-"""
 
 import os, sys, urllib, exceptions, mimetools, pycurl, optparse, datetime, hashlib
 import fuzzing.vectors
@@ -63,6 +44,7 @@ class Curl:
 	self.fakeheaders = fakeheaders
         self.headers = fakeheaders
         self.set_option(pycurl.SSL_VERIFYHOST, 2)
+        self.set_option(pycurl.SSL_VERIFYPEER, 0);
         self.set_option(pycurl.FOLLOWLOCATION, 1)
         self.set_option(pycurl.MAXREDIRS, 5)
         self.set_option(pycurl.COOKIEFILE, "/dev/null")
